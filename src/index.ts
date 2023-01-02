00a0a0b92923c2cd426e6159576768f29cebbcac
Haziq <fiqri.haziq@gmail.com>
2023-01-03 01:49:51 +0800

    testing types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import axios, { AxiosInstance } from "axios";
+import {
+  CreateCollectionRequest,
+  CreateCollectionResponse,
+  CreateOpenCollectionRequest,
+  CreateOpenCollectionResponse,
+  GetCollectionIndexResponse,
+  GetCollectionResponse,
+} from "./types/collection";
 
 export class BillPlz {
   private http: AxiosInstance;
@@ -48,29 +56,31 @@ export class BillPlz {
       });
   }
 
-  async createCollection(payload: {
-    title: string;
-    logo?: string;
-    split_payment?: {
-      email?: string;
-      fixed_cut?: number;
-      split_header?: boolean;
-    };
-  }): Promise<
-    Array<{
-      id: string;
-      title: string;
-      logo: {
-        thumb_url: string | null;
-        avatar_url: string | null;
-      };
-      split_payment: {
-        email: string | null;
-        fixed_cut: number | null;
-        split_header: boolean;
-      };
-    }>
-  > {
+  async createCollection(
+    payload: // {
+    //   title: string;
+    //   logo?: string;
+    //   split_payment?: {
+    //     email?: string;
+    //     fixed_cut?: number;
+    //     split_header?: boolean;
+    //   };
+    // }
+    CreateCollectionRequest
+  ): Promise<// Array<{
+  //   id: string;
+  //   title: string;
+  //   logo: {
+  //     thumb_url: string | null;
+  //     avatar_url: string | null;
+  //   };
+  //   split_payment: {
+  //     email: string | null;
+  //     fixed_cut: number | null;
+  //     split_header: boolean;
+  //   };
+  // }>
+  CreateCollectionResponse> {
     return await this.request({
       url: "/v3/collections",
       method: "POST",
@@ -78,20 +88,21 @@ export class BillPlz {
     });
   }
 
-  async getCollection(id: string): Promise<{
-    id: string;
-    title: string;
-    logo: {
-      thumb_url: string | null;
-      avatar_url: string | null;
-    };
-    split_payment: {
-      email: string | null;
-      fixed_cut: number | null;
-      variable_cut: number | null;
-    };
-    status: string;
-  }> {
+  async getCollection(id: string): Promise<// {
+  //   id: string;
+  //   title: string;
+  //   logo: {
+  //     thumb_url: string | null;
+  //     avatar_url: string | null;
+  //   };
+  //   split_payment: {
+  //     email: string | null;
+  //     fixed_cut: number | null;
+  //     variable_cut: number | null;
+  //   };
+  //   status: string;
+  // }
+  GetCollectionResponse> {
     return await this.request({
       url: `/v3/collections/${id}`,
       method: "GET",
@@ -101,23 +112,24 @@ export class BillPlz {
   async getCollectionIndex(
     page?: string,
     status?: string
-  ): Promise<{
-    collections: Array<{
-      id: string;
-      title: string;
-      logo: {
-        thumb_url: string | null;
-        avatar: string | null;
-      };
-      split_payment: {
-        email: string | null;
-        fixed_cut: number | null;
-        split_header: boolean;
-      };
-      status: string;
-    }>;
-    page: number;
-  }> {
+  ): Promise<// {
+  //   collections: Array<{
+  //     id: string;
+  //     title: string;
+  //     logo: {
+  //       thumb_url: string | null;
+  //       avatar: string | null;
+  //     };
+  //     split_payment: {
+  //       email: string | null;
+  //       fixed_cut: number | null;
+  //       split_header: boolean;
+  //     };
+  //     status: string;
+  //   }>;
+  //   page: number;
+  // }
+  GetCollectionIndexResponse> {
     return await this.request({
       url: "/v3/collections",
       method: "GET",
@@ -128,47 +140,51 @@ export class BillPlz {
     });
   }
 
-  async createOpenCollection(payload: {
-    title: string;
-    description: string;
-    amount: number;
-    fixed_amount?: boolean;
-    fixed_quantity?: boolean;
-    reference_1_label?: string;
-    reference_2_label?: string;
-    email_link?: string;
-    tax?: number;
-    photo?: string;
-    split_payment?: {
-      email?: string;
-      fixed_cut?: number;
-      variable_cut?: number;
-      split_header?: boolean;
-    };
-  }): Promise<{
-    id: string;
-    title: string;
-    description: string;
-    reference_1_label: string | null;
-    reference_2_label: string | null;
-    email_link: string | null;
-    amount: number | null;
-    fixed_amount: boolean;
-    fixed_quantity: boolean;
-    tax: number | null;
-    payment_button: string;
-    photo: {
-      retina_url: string | null;
-      avatar_url: string | null;
-    };
-    split_payment: {
-      email: string | null;
-      fixed_cut: number | null;
-      variable_cut: number | null;
-      split_header: boolean;
-    };
-    url: string;
-  }> {
+  async createOpenCollection(
+    payload: //   {
+    //   title: string;
+    //   description: string;
+    //   amount: number;
+    //   fixed_amount?: boolean;
+    //   fixed_quantity?: boolean;
+    //   reference_1_label?: string;
+    //   reference_2_label?: string;
+    //   email_link?: string;
+    //   tax?: number;
+    //   photo?: string;
+    //   split_payment?: {
+    //     email?: string;
+    //     fixed_cut?: number;
+    //     variable_cut?: number;
+    //     split_header?: boolean;
+    //   };
+    // }
+    CreateOpenCollectionRequest
+  ): Promise<// {
+  //   id: string;
+  //   title: string;
+  //   description: string;
+  //   reference_1_label: string | null;
+  //   reference_2_label: string | null;
+  //   email_link: string | null;
+  //   amount: number | null;
+  //   fixed_amount: boolean;
+  //   fixed_quantity: boolean;
+  //   tax: number | null;
+  //   payment_button: string;
+  //   photo: {
+  //     retina_url: string | null;
+  //     avatar_url: string | null;
+  //   };
+  //   split_payment: {
+  //     email: string | null;
+  //     fixed_cut: number | null;
+  //     variable_cut: number | null;
+  //     split_header: boolean;
+  //   };
+  //   url: string;
+  // }
+  CreateOpenCollectionResponse> {
     return await this.request({
       url: "/v3/open_collections",
       method: "POST",
@@ -369,3 +385,16 @@ export class BillPlz {
 
   // TODO: add v4 endpointss
 }
+
+const BillPlzInstance = new BillPlz({
+  key: "4c0157d7-00cc-46dd-bab9-afef6f573f7c",
+  sandbox: true,
+});
+
+// const getCollection = await BillPlzInstance.getCollection("vnzd6hpm");
+// console.log(getCollection);
+
+const createCollection = await BillPlzInstance.createCollection({
+  title: "Test Collection",
+});
+console.log(createCollection);
